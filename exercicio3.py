325e482165eae197dd7d3c6c568c0d274c9e78c4
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2019-10-30 00:23:24 -0400

    Exercício 3 - Tokenizador 1.0

--- exercicio3.py
+++ exercicio3.py
@@ -1,24 +1,28 @@
 codigo = open("/mnt/c/Users/samuel/Documents/ufmt/compiladores1/entradaExercicio3.txt", "r").read()
 codigo = codigo.replace("(", "( ")
 codigo = codigo.replace(")", " )")
+codigo = codigo.replace("\n", " malandragi ")
 tokens = codigo.split()
 
 palavraReservada = ['if']
 operadorRelacional = ['<']
 operadorAtribuição = ['=']
 operadorAritmetico = ['+', '-', '*', '/']
+numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 aux1 = 0
+aux2 = 0
 variavil = " "
+variavil2 = " "
 for token in tokens:
     if token in palavraReservada:
         print(token, ", palavra reservada")
     elif token  == '(':
-        print(token + ", abre parenteses")
+        print(token + ", abre parênteses")
     elif token in operadorRelacional:
         print(token + ", operador relacional")
     elif token == ')':
-        print(token + ", fecha parenteses")
+        print(token + ", fecha parênteses")
     elif token == '{':
         print(token + ", abre chaves")
     elif token == '}':
@@ -36,7 +40,17 @@ for token in tokens:
         variavil += " */"
         aux1 = 0
         print(variavil + ", comentário")
-    
+    elif token == "//":
+        variavil2 = "//"
+        aux2 = 1
+    elif aux2 == 1 and token != "malandragi":
+        variavil2 += token
+    elif aux2 == 1 and token == "malandragi":
+        aux2 = 0
+        print(variavil2 + ", comentário")
+    elif token[0] not in numeros and token != "malandragi":
+        print(token + ", identificador")
+
     try:
         int(token)
         print(token + ", numero inteiro")
