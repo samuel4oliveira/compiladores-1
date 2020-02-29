42dab1eb9a120180d06aa281b9be8cfa129281b0
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2020-02-29 08:39:52 -0400

    Adicionando erro ao Analisador Lexico

--- analisadorLexico.py
+++ analisadorLexico.py
@@ -1,25 +1,28 @@
-palavrasReservadas = {
-    'var':'declaracao',
-    ':' : 'dois pontos',
-    ',':'virgula',
-    'integer':'tipo',
-    ';':'ponto e virgula',
-    ':=':'atribuicao',
-    'if':'condicional',
-    'then':'acao condicional',
-    '+':'soma'
-}
+def analisadorLexico(pathEntrada):
+    palavrasReservadas = {
+        'var':'declaracao',
+        ':' : 'dois pontos',
+        ',':'virgula',
+        'integer':'tipo',
+        ';':'ponto e virgula',
+        ':=':'atribuicao',
+        'if':'condicional',
+        'then':'acao condicional',
+        '+':'soma'
+    }
 
-path = '/home/samuel/Documents/ufmt/entrada-parte2.txt'
-arquivo = open(path).read()
-for x in palavrasReservadas:
-    arquivo = arquivo.replace(x, " " + x + " ")
-arquivo = arquivo.replace(': =', ':=')
-tokens = arquivo.split()
+    arquivo = open(pathEntrada).read()
+    for x in palavrasReservadas:
+        arquivo = arquivo.replace(x, " " + x + " ")
+    arquivo = arquivo.replace(': =', ':=')
+    tokens = arquivo.split()
 
-resultado = {}
-for token in tokens:
-    if token in palavrasReservadas:
-        resultado.update({token:palavrasReservadas[token]})
-    else:
-        resultado.update({token:'identificador'})
+    resultado = {}
+    for token in tokens:
+        if token in palavrasReservadas:
+            resultado.update({token:palavrasReservadas[token]})
+        elif token.isalpha():
+            resultado.update({token:'identificador'})
+        else:
+            return ('Erro') 
+    return resultado
