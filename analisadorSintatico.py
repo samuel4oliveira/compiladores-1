4851989709f24b8fa4795503c4bb5d99db92333c
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2020-03-06 11:07:36 -0400

    FIX - Analisador Sintático e Entrada

--- analisadorSintatico.py
+++ analisadorSintatico.py
@@ -3,7 +3,8 @@ import analisadorLexico
 global posicao, token
 posicao = 0
 token = analisadorLexico.analisador_lexico('/home/samuel/Documents/ufmt/compiladores1/entrada.txt')
-
+for i in range(len(token)):
+    print(i, token[i])
 def analisador_sintatico():
 
     def z():
@@ -12,6 +13,7 @@ def analisador_sintatico():
     
     def i():
         global posicao, token
+        print(posicao, 'i')
         if token[posicao][1] == 'declaracao':
             posicao += 1
             d()
@@ -20,6 +22,7 @@ def analisador_sintatico():
     
     def d():
         global posicao, token
+        print(posicao, 'd')
         l()
         if token[posicao][1] == 'dois pontos':
             posicao += 1
@@ -28,6 +31,7 @@ def analisador_sintatico():
         
     def l():
         global posicao, token
+        print(posicao, 'l')
         if token[posicao][1] == 'identificador':
             posicao += 1
             x()
@@ -36,12 +40,14 @@ def analisador_sintatico():
     
     def x():
         global posicao, token
+        print(posicao, 'x')
         if token[posicao][1] == 'virgula':
             posicao += 1
             l()
     
     def k():
         global posicao, token
+        print(posicao, 'k')
         if token[posicao][1] == 'tipo':
             posicao += 1
         else:
@@ -49,21 +55,32 @@ def analisador_sintatico():
 
     def o():
         global posicao, token
+        print(posicao, 'o')
         if token[posicao][1] == 'ponto e virgula':
             posicao += 1
             d()
     
     def s():
         global posicao, token
-        if token[posicao-1][1] == 'identificador':
+        print(posicao, 's')
+        if token[posicao][1] == 'identificador':
+            print(posicao, 'identificador')
+            posicao += 1
             if token[posicao][1] == 'atribuicao':
+                print(posicao, 'atribuicao')
                 posicao += 1
                 e()
+                if posicao < len(token):
+                    raise EnvironmentError("Erro Sintático, declaração após atribuição.")
             else:
                 raise EnvironmentError("Erro Sintático, é esperado 'var', '" + token[posicao][0] + "' não é válido.")
         elif token[posicao][1] == 'condicional':
+            posicao += 1
+            print(posicao, 'condicional')
             e()
             if token[posicao][1] == 'acao condicional':
+                posicao += 1
+                print(posicao, 'acao condicional')
                 s()
             else:
                 raise EnvironmentError("Erro Sintático, é esperado 'then', '" + token[posicao][0] + "' não é válido.")
@@ -72,11 +89,13 @@ def analisador_sintatico():
     
     def e():
         global posicao, token
+        print(posicao, 'e')
         t()
         r()
     
     def r():
         global posicao, token
+        print(posicao, 'r')
         try:
             if token[posicao][1] == 'soma':
                 posicao += 1
@@ -87,10 +106,9 @@ def analisador_sintatico():
     
     def t():
         global posicao, token
+        print(posicao, 't') 
         if token[posicao][1] == 'identificador':
             posicao += 1
-        else:
-            raise EnvironmentError("Erro Sintático, é esperado 'identificador', '" + token[posicao][0] + "' não é válido.")
 
     z() 
 analisador_sintatico()
