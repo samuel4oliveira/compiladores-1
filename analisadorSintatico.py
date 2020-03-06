5602ee570238b2e2711a43649a644fe03a6844e5
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2020-03-06 02:07:23 -0400

    FIX - Analisador Sintático e Entrada

--- analisadorSintatico.py
+++ analisadorSintatico.py
@@ -1,100 +1,96 @@
 import analisadorLexico
 
-def analisador_sintatico(tokens):
+global posicao, token
+posicao = 0
+token = analisadorLexico.analisador_lexico('/home/samuel/Documents/ufmt/compiladores1/entrada.txt')
 
-    global posicao 
-    posicao = 0
+def analisador_sintatico():
 
-    def r(tokens):
-        global posicao
-        try:
-            if tokens[posicao][1] == 'soma':
-                posicao += 1
-                t(tokens)
-                r(tokens)
-        except IndexError:
-            return
-
-    def t(tokens):
-        global posicao
-        if tokens[posicao][1] == 'identificador':
-            posicao += 1
-        else:
-            raise ValueError("Erro de Sintaxe, '" + tokens[posicao][0] + "' não é um identificador válido." )
-
-    def e(tokens):
-        global posicao
-        t(tokens)
-        r(tokens)
-
-    def s(tokens):
-        global posicao
-        if tokens[posicao][1] == 'identificador' and tokens[posicao+1][1] == 'atribuicao':
-            posicao += 2
-            e(tokens)
-            if posicao > len(tokens):
-                return
-        try:
-            if tokens[posicao][1] == 'condicional':
-                posicao += 1
-                e(tokens)
-                if tokens[posicao][1] == 'acao condicional':
-                    posicao += 1 
-                    s(tokens)
-                else:
-                    raise ValueError("Erro de Sintaxe, '" + tokens[posicao][0] + "' não é um operador de ação condicional." )
-            else:
-                raise ValueError("Erro de Sintaxe, '" + tokens[posicao][0] + "' não é um operador condicional.")
-        except IndexError:
-            return
-
-    def o(tokens):
-        global posicao
-        if tokens[posicao][1] == 'ponto e virgula':
-            posicao += 1
-            d(tokens)
-
-    def k(tokens):
-        global posicao
-        if tokens[posicao][1] == 'tipo':
-            posicao += 1
-        else:
-            raise ValueError("Erro de Sintaxe, '" + tokens[posicao][0] + "' não é um tipo suportado.")
-
-    def x(tokens):
-        global posicao
-        if tokens[posicao][1] == 'virgula':
-            posicao += 1
-            l(tokens)
-
-    def l(tokens):
-        global posicao
-        if tokens[posicao][1] == 'identificador':
-            posicao += 1
-            x(tokens)
-        else:
-            raise ValueError("Erro de Sintaxe, '" + tokens[posicao][0] + "' não é um identificador válido.")
+    def z():
+        i()
+        s()
     
-    def d(tokens):
-        global posicao
-        l(tokens)
-        if  tokens[posicao][1] == 'dois pontos':
+    def i():
+        global posicao, token
+        if token[posicao][1] == 'declaracao':
             posicao += 1
-            k(tokens)
-            o(tokens)
+            d()
         else:
-            raise ValueError("Erro de Sintaxe, era esperado ':', " + tokens[posicao][0] + " não é válido.")
+            raise EnvironmentError("Erro Sintático, é esperado 'var', '" + token[posicao][0] + "' não é válido.")
+    
+    def d():
+        global posicao, token
+        l()
+        if token[posicao][1] == 'dois pontos':
+            posicao += 1
+            k()
+            o()
         
-    def i(tokens):
-        global posicao
-        if tokens[posicao][1] == 'declaracao':
+    def l():
+        global posicao, token
+        if token[posicao][1] == 'identificador':
             posicao += 1
-            d(tokens)
+            x()
         else:
-            raise ValueError("Erro de Sintaxe, era esperado 'var', " + tokens[posicao][0] + " não é válido.")
+            raise EnvironmentError("Erro Sintático, é esperado 'identificador', '" + token[posicao][0] + "' não é válido.")
+    
+    def x():
+        global posicao, token
+        if token[posicao][1] == 'virgula':
+            posicao += 1
+            l()
+    
+    def k():
+        global posicao, token
+        if token[posicao][1] == 'tipo':
+            posicao += 1
+        else:
+            raise EnvironmentError("Erro Sintático, é esperado 'integer' ou 'real', '" + token[posicao][0] + "' não é válido.")
 
-    def z(tokens):
-        i(tokens)
-        s(tokens)
-   
-    z(tokens)
+    def o():
+        global posicao, token
+        if token[posicao][1] == 'ponto e virgula':
+            posicao += 1
+            d()
+    
+    def s():
+        global posicao, token
+        if token[posicao-1][1] == 'identificador':
+            if token[posicao][1] == 'atribuicao':
+                posicao += 1
+                e()
+            else:
+                raise EnvironmentError("Erro Sintático, é esperado 'var', '" + token[posicao][0] + "' não é válido.")
+        elif token[posicao][1] == 'condicional':
+            e()
+            if token[posicao][1] == 'acao condicional':
+                s()
+            else:
+                raise EnvironmentError("Erro Sintático, é esperado 'then', '" + token[posicao][0] + "' não é válido.")
+        else:
+            raise EnvironmentError("Erro Sintático, é esperado 'identificador' ou 'if', '" + token[posicao][0] + "' não é válido.")
+    
+    def e():
+        global posicao, token
+        t()
+        r()
+    
+    def r():
+        global posicao, token
+        try:
+            if token[posicao][1] == 'soma':
+                posicao += 1
+                t()
+                r()
+        except IndexError:
+            return
+    
+    def t():
+        global posicao, token
+        if token[posicao][1] == 'identificador':
+            posicao += 1
+        else:
+            raise EnvironmentError("Erro Sintático, é esperado 'identificador', '" + token[posicao][0] + "' não é válido.")
+
+    z() 
+analisador_sintatico()
